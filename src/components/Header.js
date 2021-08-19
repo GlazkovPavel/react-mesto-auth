bb930299da1ae52c960c43e344276c015b560c60
GlazkovPavel <glazprom@mail.ru>
2021-08-19 19:46:44 +0300

    feat: Кнопка входа

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ function Header() {
     return(
         <header className="header">
             <img className="header__logo" src={logo} alt="Место" />
+          <button className="header__button">Войти</button>
         </header>
     )
 }
