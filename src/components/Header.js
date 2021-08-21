680afad139a2d62ecdad059dfd3c193851236de6
GlazkovPavel <glazprom@mail.ru>
2021-08-21 20:24:01 +0300

    feat: сделал роуты

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,26 @@
 import React from "react";
 import logo from "../images/mesto.svg";
-import {AuthContext} from "../contexts/AuthContext";
-import {useLocation} from "react-router-dom";
+import { Route, Link} from "react-router-dom";
 
 
 
 function Header() {
-  const isLoggedIn = React.useContext(AuthContext)
-  let location = useLocation();
-  console.log(location);
+
   return(
         <header className="header">
           <img className="header__logo" src={logo} alt="Место" />
-          <div className="header__container">
-            <p className="header__email">{ isLoggedIn ? 'Email' : ''}</p>
-            <button className="header__button">{ isLoggedIn ? 'Выйти' : 'Войти'}</button>
-          </div>
-
+          <Route path="/sign-up">
+            <Link to="/sign-in" className="header__button">Войти</Link>
+          </Route>
+          <Route path="/sign-in">
+            <Link to="/sign-up" className="header__button">Регистрация</Link>
+          </Route>
+          <Route exact path="/">
+            <div className="header__container">
+              <p className="header__email">Email</p>
+              <button className="header__button">Выйти</button>
+            </div>
+          </Route>
         </header>
     )
 }
